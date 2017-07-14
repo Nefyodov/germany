e29e986eadfe9e2dee63e41c9eab3d008d2a4984
unknown <d.nefedov@ws-kv-fin01.fish.corp>
2017-07-14 17:59:12 +0300

    require [key]

--- inc/center/income.php
+++ inc/center/income.php
@@ -1,7 +1,17 @@
-<select class="form-control">
-    <?php
-    for ($i=1; $i<=12;$i++) {
-        echo '<option>' . date('F',mktime(0,0,0,$i)) .  '</option>';
-    }
-    ?>
+<form action="" name="month" method="post">
+    <select class="form-control">
+        <?php
+        for ($i=1; $i<=12;$i++) {
+            $month = date('F',mktime(0,0,0,$i));
+            echo '<option value="'.$month .'">' . $month .  '</option>';
+            echo "\n\t\t";
+        }
+        ?>
 </select>
+
+    <button class="btn btn-default">Submit</button>
+</form>
+
+<?php
+
+echo $_POST['month'];
